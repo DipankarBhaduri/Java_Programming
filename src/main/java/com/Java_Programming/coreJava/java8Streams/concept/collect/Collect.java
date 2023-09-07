a8f18f56d6e0ead26baf3b74eef7c26fd0b35f09
Dipankar Bhaduri <110708060+DipankarBhaduri@users.noreply.github.com>
2023-09-07 13:04:30 +0530

    count, Builder, Concat, collect add

---
+++ src/main/java/com/Java_Programming/coreJava/java8Streams/concept/collect/Collect.java
@@ -0,0 +1,21 @@
+package com.Java_Programming.coreJava.java8Streams.concept.collect;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Collect {
+    /**
+     * @Questions : Given a list of exam scores, determine if all student has scored above a certain threshold
+     * @param args
+     */
+    public static void main(String[] args) {
+        List<Integer> scores = Arrays.asList(76, 87, 78, 77);
+        List<Integer> list = doubleAllTheElement (scores);
+        list.stream().forEach(System.out::println);
+    }
+
+    private static List<Integer> doubleAllTheElement(List<Integer> scores) {
+        return scores.stream().map(score -> score * 2).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/Java_Programming/coreJava/java8Streams/concept/count/Count.java
@@ -0,0 +1,7 @@
+package com.Java_Programming.coreJava.java8Streams.concept.count;
+
+public class Count {
+    public static void main(String[] args) {
+
+    }
+}
